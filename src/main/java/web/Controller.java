6bce5482b8d560903be2a1c12098270718bc7559
111408037 <mrunalpatil035@gmail.com>
2020-01-18 18:45:48 +0530

    code changes..

---
+++ src/main/java/web/Controller.java
@@ -0,0 +1,27 @@
+package web;
+
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Autowired
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/getServiceName")
+    public String getServiceName(){
+        return String.format("Hello from %s running gon port %s!!", eurekaClient.getApplication(appName).getName(), port);
+    }
+
+
+}
